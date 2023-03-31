98e3408409fd932d9861755fedff17943f87b3df
then0403 <39440926+then0403@users.noreply.github.com>
2023-03-30 19:14:41 -0700

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Layout from '../components/Layout'
+import LoginModal from '../components/modals/LoginModal'
+import RegisterModal from '../components/modals/RegisterModal'
+
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <RegisterModal />
+      <LoginModal />
+      <Layout>
+      <Component {...pageProps} />
+      </Layout>
+    </>
+  )  
 }
 
 export default MyApp
